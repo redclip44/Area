0ad88667f585176e30501ccd7f9c6d152f2d04d5
redclip44 <sparetempextra@gmail.com>
2023-08-07 09:56:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pattern
 
 def main():
     mem = ressources.Addresses.GameClass("saplogon.exe")
-    bytes_pattern = b".\x50\x00\x53\x00\x47\x00\x57\x00\x57\x00\x4F\x00\x3A\x00\x20\x00\x30\x00" \
+    bytes_pattern = b".\x50\.\.\.\.\x00\x57\x00\.\.\.\.\.\x00\x3A\x00\x20\x00\x30\x00" \
             b"\x37\x00\x20\x00\x2D\x00\x20\x00\x32\x00\x37\x00"
     addr = pattern.pattern_scan_all(mem.handle,bytes_pattern)
     mem.readBytes(addr,20)
